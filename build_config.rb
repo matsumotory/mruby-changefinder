46455c20eceef751498075c7c31393ac47b3e249
matsumoto-r <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2016-04-07 10:47:04 +0900

    Remove unused gem for test

--- build_config.rb
+++ build_config.rb
@@ -2,9 +2,8 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 
   # include the default GEMs
-  conf.gembox 'full-core'
+  conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
-  conf.gem :github => 'matsumoto-r/mruby-sleep'
   conf.enable_test
 end
 
